61bf6a9e7b7c8818dee0826aa43b609ee213f62a
Jorge Romero <jorge.romero.ext@boehringer-ingelheim.com>
2026-03-25 16:16:44 +0100

    Refactor import statements in ApiDefinitionEntity and AuthorizationPolicyEntity for consistency

--- persistence/src/main/java/org/opendevstack/apiservice/persistence/entity/ApiDefinitionEntity.java
+++ persistence/src/main/java/org/opendevstack/apiservice/persistence/entity/ApiDefinitionEntity.java
@@ -1,9 +1,14 @@
 package org.opendevstack.apiservice.persistence.entity;
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
