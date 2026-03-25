09fca8b4767056e4106a87e9bfdba4c87b05970d
Jorge Romero <jorge.romero.ext@boehringer-ingelheim.com>
2026-03-25 16:56:24 +0100

    Remove unused imports from ClientAppEntity for cleaner code

--- persistence/src/main/java/org/opendevstack/apiservice/persistence/entity/ClientAppEntity.java
+++ persistence/src/main/java/org/opendevstack/apiservice/persistence/entity/ClientAppEntity.java
@@ -1,25 +1,20 @@
 package org.opendevstack.apiservice.persistence.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import java.time.OffsetDateTime;
-import java.util.LinkedHashSet;
-import java.util.Set;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 /**
  * JPA entity mapping the {@code client_apps} table.
